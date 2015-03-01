9302494e70fffabc4e86ec222193ec8567b590cd
FelipeMello <felipeourjake@outlook.com>
2015-02-28 23:58:47 -0300

    base0.0.1
    base0.0.1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <?php include'mod_meta/index.php'?>
+        
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+        <!--HTML5-->
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+
+        <!--Códigos Entre o Head-->
+        <?php include'mod_codigo_head/index.php'?>
+        
+    </head>
+    <body>
+        <!--Códigos Começo do Body-->
+        <?php include'mod_codigo_comeco_body/index.php'?>
+        <!--#############################################################-->
+        
+        
+        
+        <!--#############################################################-->
+        <!--JS-->
+        <script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+        
+        <!--Códigos Final do Body-->
+        <?php include'mod_codigo_fim_body/index.php'?>
+        
+        <!--#############################################################-->
+    </body>
+</html>
---
+++ mod_meta/index.php
@@ -0,0 +1,15 @@
+<title></title>
+        <!--META TAGS-->
+        <meta charset="utf-8" />
+        <meta name="description" content="" />
+        <meta name="author" content="FOXID | Soluções de Marketing Digital" />
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+        <!--LINK-->
+        <link rel="shortcut icon" type="image/x-icon" href="imagens/favicon/favicon.png" />
+        <link rel="canonical" href="" />
+        <!--FACEBOOK-->
+        <meta property="og:type" content="website" />
+        <meta property='og:locale' content='pt_BR'/>
+        <meta property="og:image" content="" />
+        <meta property='og:title' content="" />
+        <meta property='og:url' content="" />
